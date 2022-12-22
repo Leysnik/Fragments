42aab624550aaf235485fad208d8d24349f9d3ec
Leysnik <77565343+Leysnik@users.noreply.github.com>
2022-12-23 02:04:06 +0800

    FragmentExample app with 3 Fragments and File

---
+++ app/src/main/java/com/example/fragments/FinalTask.java
@@ -0,0 +1,75 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FinalTask#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FinalTask extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    ArrayList<String> tasks;
+    TextView cList;
+
+    public FinalTask(ArrayList<String > s) {
+        tasks = s;
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FinalTask.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FinalTask newInstance(String param1, String param2) {
+        FinalTask fragment = new FinalTask(new ArrayList<>());
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_final_task, container, false);
+        cList = v.findViewById(R.id.final_list_task);
+        for (int i = 0; i < tasks.size(); i++) {
+            String tmp = tasks.get(i) + "\n";
+            cList.append(tmp);
+        }
+        return v;
+    }
+}
